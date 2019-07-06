662b6047f6ee1516859be2d63259d45162f64ea8
kv-2 <str_token@mail.ru>
2019-07-06 15:38:18 +0300

    populate the cintroller
    Rails provides a hash with the name cookies[]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 class HomeController < ApplicationController
+  # Rails provides a hash with the name cookies[]
   def set_cookies
+    cookies[:user_name] = "Smith"
+    cookies[:customes_number] = "1234567890"
   end
 
   def show_cookies
+    @user_name = cookies[:user_name]
+    @customes_number = cookies[:customes_number]
   end
 
   def delete_cookies
+    cookies.delete :user_name
+    cookies.delete :customes_number
   end
 end
